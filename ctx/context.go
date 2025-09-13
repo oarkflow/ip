919a9062b5d7720a19982c4294fadd981b86693b
sujit <s.baniya.np@gmail.com>
2025-09-13 14:22:47 +0545

    update

--- ctx/context.go
+++ ctx/context.go
@@ -3,7 +3,7 @@ package ctx
 type Context interface {
 	Set(key string, value any)
 	Next() error
-	Get(key string) string
+	Get(key string, def ...string) string
 	IP() string
 	Locals(key any, value ...any) any
 	JSON(data any, ctype ...string) error
